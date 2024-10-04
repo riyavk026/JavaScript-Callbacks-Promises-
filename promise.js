a1da9aa4204609dabf88c4e9a51446d433a7b0f0
Riya <riyavk026@gmail.com>
2024-10-04 14:47:29 +0530

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,40 @@
+async function sleep() {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(45)
+    }, 1000);
+})
+}
+
+function sum (a,b,c){
+    return a+b+c
+}
+(async function main(){
+    console.log(a1)
+//     let a = await sleep()
+//     console.log(a)
+//     let b = await sleep()
+//     console.log(b)
+//    
+
+let obj = {
+    a :1,
+    b :2,
+    c :3,
+}
+
+let {a, b} = obj
+console.log (a,b)
+})()
+
+let arr = [1,2,3]
+console.log(arr[0]+arr[1]+arr[2])
+console.log (sum(...arr))
+
+var a1 = 5
+
+    
+
+
+
+
